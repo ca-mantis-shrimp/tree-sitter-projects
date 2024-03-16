9e7aa75f091fd7d2894d11fb9b4c0a6eb9a90ad3
Darrion Burgess <dargondab9@gmail.com>
2024-03-16 00:17:49 -0700

    add our first fold and indent version!

---
+++ queries/projects/folds.scm
@@ -0,0 +1,5 @@
+(root_project) @fold
+(child_project) @fold
+(grandchild_project) @fold
+(great_grandchild_project) @fold
+(root_project) @fold
---
+++ queries/projects/indents.scm
@@ -0,0 +1,11 @@
+ ((root_project) @indent.begin
+   (#set! indent.immediate 1))
+
+ ((child_project) @indent.begin
+   (#set! indent.immediate 1))
+
+ ((grandchild_project) @indent.begin
+   (#set! indent.immediate 1))
+
+ ((great_grandchild_project) @indent.begin
+   (#set! indent.immediate 1))
